cf7e24a92b419c6e92a8579bdae6e9af25ec87f9
Toraiex <toraiex007@gmail.com>
2026-02-25 11:01:57 +0700

    LoginOK

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,8 +4,9 @@ import (
 	database "simple-clothes-shop/pkg/database"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 type App struct {
@@ -21,14 +22,13 @@ func NewApp() *App {
 	app := fiber.New()
 
 	app.Use(
-		cors.New(cors.Config{
-			AllowOrigins: "*",
-			AllowMethods: "GET,POST,PATCH,DELETE,PUT",
-		}),
+		requestid.New(), // สร้าง ID ให้ทุก Request
 		logger.New(logger.Config{
+			Format:     "${time} | ${pid} | ${locals:requestid} | ${status} | ${latency} | ${method} | ${path}\n",
 			TimeFormat: "2006-01-02 15:04:05",
 			TimeZone:   "Asia/Bangkok",
 		}),
+		recover.New(),
 	)
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Server Running")
